8f10ccaf141932d69ce6d45c5b9be7d75311d0de
ty1295 <ty1295@naver.com>
2021-09-23 16:22:29 +0900

    Fix: 백엔드 UserController 수정

--- backend/src/main/java/com/mycom/app/controller/UserController.java
+++ backend/src/main/java/com/mycom/app/controller/UserController.java
@@ -2,7 +2,6 @@ package com.mycom.app.controller;
 
 import com.mycom.app.domain.dto.UserDto;
 import com.mycom.app.service.UserService;
-import com.sun.org.apache.regexp.internal.RE;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;

aa4c7797a53bd0da318e12934e02ecb3dfbc6939
DESKTOP-KVCQHCD\multicampus <jm9521@hanmail.net>
2021-09-28 23:51:47 +0900

    S05P21A107-225 Feat: 메인 페이지 디자인 #1

--- frontend/src/views/mainPage/index.jsx
+++ frontend/src/views/mainPage/index.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import play from '../../assets/mainphoto.png'
+import test from '../../assets/test.png'
+import test2 from '../../assets/test2.png'
 import '../../styles/main.css';
 const MainPage = (props) => {
   
   return (
     <>
       <div className="main-container">
-        <div className="main-image">
+        <div className="test"></div>
+        {/* <div className="test2"><img src={test} alt="" className="test3"></img></div> */}
+        <div className="test2"><img src={test2} alt="" className="test3"></img></div>
+        <div className="test4"></div>
+        <div className="test5"></div>
+
+        <div className="main-desc">
+          <h1>알짜배기</h1>
+          <h5 className="desc">게임으로 배우는 알고리즘과 자료구조</h5>
+          <button className="startBtn">시작하기</button>
+        </div>
+
+
+        {/* <div className="main-image">
           <img src={play} alt=""/>
         </div>
         <div className="main-desc">
-          <h1>E-Run</h1>
-          <h5>자료구조로 게임을 재밌게 배워보세요</h5>
-          <button>시작하기</button>
-        </div>
+          <h1>알짜배기</h1>
+          <h5 className="desc">게임으로 배우는 알고리즘과 자료구조</h5>
+          <button className="startBtn">시작하기</button>
+        </div> */}
       </div>
     </>
   )

49d2d5ea60eb1d4576d18f6a26e1ce2555259238
DESKTOP-KVCQHCD\multicampus <jm9521@hanmail.net>
2021-09-29 10:04:29 +0900

    S05P21A107-225 Feat: 메인페이지 디자인 #2

--- frontend/src/views/mainPage/index.jsx
+++ frontend/src/views/mainPage/index.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import play from '../../assets/mainphoto.png'
-import test from '../../assets/test.png'
 import test2 from '../../assets/test2.png'
 import '../../styles/main.css';
+
 const MainPage = (props) => {
   
   return (
@@ -12,13 +11,28 @@ const MainPage = (props) => {
         {/* <div className="test2"><img src={test} alt="" className="test3"></img></div> */}
         <div className="test2"><img src={test2} alt="" className="test3"></img></div>
         <div className="test4"></div>
-        <div className="test5"></div>
+        <div className="test5 bounce"></div>
 
-        <div className="main-desc">
+        <div className="test-desc">
           <h1>알짜배기</h1>
           <h5 className="desc">게임으로 배우는 알고리즘과 자료구조</h5>
           <button className="startBtn">시작하기</button>
         </div>
+
+        <section class="flex scene">
+        <div class="confetti">
+          <figure class="confetti__image">
+            <svg viewBox="-225 -225 450 450" role="presentational" />
+          </figure>
+          <button class="button confetti__button" type="button">
+            <span class="button__text">
+              click me
+            </span>
+          </button>
+        </div>
+      </section>
+
+        
 
 
         {/* <div className="main-image">

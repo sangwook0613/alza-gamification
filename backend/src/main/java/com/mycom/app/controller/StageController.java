a365095d970058457f7bba806f1bb49869b49818
조의상 <ourjes@naver.com>
2021-10-01 22:02:09 +0900

    S05P21A107-185 Feat : Stage 클리어 정보 연동 요청 #2 (매핑 URL 변경)

--- backend/src/main/java/com/mycom/app/controller/StageController.java
+++ backend/src/main/java/com/mycom/app/controller/StageController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 
-@RequestMapping("/stage")
+@RequestMapping("/api/stage")
 //@RequestMapping("/api/stage")
 public class StageController {
 

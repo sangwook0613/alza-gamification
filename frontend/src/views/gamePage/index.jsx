c79b5260dd5258821262ccbac3427a540d242052
DESKTOP-KVCQHCD\multicampus <jm9521@hanmail.net>
2021-10-02 22:09:21 +0900

    S05P21A107-227 Feat: 게임목록페페이지 반응형 구현 #1

--- frontend/src/views/gamePage/index.jsx
+++ frontend/src/views/gamePage/index.jsx
@@ -7,13 +7,18 @@ import graph from '../../assets/game/graph.png'
 import tree from '../../assets/game/tree.png'
 import sort from '../../assets/game/sort.png'
 import mid from '../../assets/game/mid.png'
+import { useMediaQuery } from 'react-responsive'
 
 
 function GamePage(props) {
+
+  const isDesktopOrLaptop = useMediaQuery({ minDeviceWidth: 800 })
+  const isTabletOrMobile = useMediaQuery({ maxWidth: 800 })
+
   const ds = [
     {id: 1, title: '스택: 별과자 완전 범죄', sort: 'Stack', img: stack, content: '형의 별과자가 너무 탐났던 사피. 음성인식 로봇 알자의 도움을 받아 몰래 훔쳐먹자! 훔쳐먹고 나서 후환이 두려워진 사피. 알자의 도움을 받아 과자를 원상복구 하자!'},
     {id: 2, title: '큐: 산타를 도와라', sort: 'Queue', img: queue, content: '크리스마스를 앞두고 한창 준비 중인 산타마을. 컨베이어 벨트가 끊어져 선물들이 다 섞여버렸다! 산타를 도와 문제를 해결하고 큐 개념을 같이 학습해봐요!!' },
-    {id: 3, title: '그래프: 완전 그래프 완성시키기', sort: 'Graph', img: graph, content: '동물들이 길을 잃고 섞여버렸다! 우리 올바른 순서대로 동물들을 정렬시켜볼까요? 아쉽게도 간장이는 다음 시즌에서 만나보실 수 있습니다. 양념이와 치킨이 정렬!' },
+    {id: 3, title: '그래프: 완전 그래프 완성', sort: 'Graph', img: graph, content: '동물들이 길을 잃고 섞여버렸다! 우리 올바른 순서대로 동물들을 정렬시켜볼까요? 아쉽게도 간장이는 다음 시즌에서 만나보실 수 있습니다. 양념이와 치킨이 정렬!' },
     {id: 4, title: '트리: 공통 조상 찾기', sort: 'Tree', img: tree, content: '동물들이 길을 잃고 섞여버렸다! 우리 올바른 순서대로 동물들을 정렬시켜볼까요? 아쉽게도 간장이는 다음 시즌에서 만나보실 수 있습니다. 양념이와 치킨이 정렬!' },
   ]
 
@@ -24,6 +29,9 @@ function GamePage(props) {
 
   let array = [];
   let array2 = [];
+  //반응형 array
+  let array_res = [];
+  let array2_res = [];
 
   if (props.match.params.category === "ds") {
     for (let i = 0; i < ds.length; i++) {
@@ -56,11 +64,47 @@ function GamePage(props) {
             <hr />
           </div>
       )
+      array_res.push(
+          <div className="responsive card">
+            <div className="image">
+              <img src={ds[i].img} alt=""></img>
+            </div>
+            <div className="details">
+              <div className="center">
+                <div className="detail-title">
+                  <h1>{ds[i].title}</h1>
+                </div>
+                <div className="detail-desc">
+                  <p>{ds[i].content}</p>
+                </div>
+                <div className="detail-btn">
+                  <Link to={{
+                    pathname: `ds/${ds[i].id}`,
+                    state: {
+                      ds: ds,
+                      algo: algo
+                    }
+                  }} className="link">
+                    <button className="custom">
+                      <i className="fas fa-play fa-xs"></i>
+                      <span className="text">Start</span>
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+      )
     }
     return (
       <div className="gamelist-container">
         <h1>자료구조</h1>
-        {array}
+        { isDesktopOrLaptop &&
+        array
+        }
+        { isTabletOrMobile &&
+        array_res
+        }
       </div>
     )
   } else if (props.match.params.category === "algo") {
@@ -94,11 +138,47 @@ function GamePage(props) {
             <hr />	       
           </div>
       )
+      array2_res.push(
+          <div className="responsive card">
+            <div className="image">
+              <img src={algo[i].img} alt=""></img>
+            </div>
+            <div className="details">
+              <div className="center">
+                <div className="detail-title">
+                  <h1>{algo[i].title}</h1>
+                </div>
+                <div className="detail-desc">
+                  <p>{algo[i].content}</p>
+                </div>
+                <div className="detail-btn">
+                  <Link to={{
+                    pathname: `algo/${algo[i].id}`,
+                    state: {
+                      ds: ds,
+                      algo: algo
+                    }
+                  }} className="link">
+                    <button className="custom">
+                      <i className="fas fa-play fa-xs"></i>
+                      <span className="text">Start</span>
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+      )
     }
     return (
       <div className="gamelist-container">
         <h1>알고리즘</h1>
-        {array2}
+        { isDesktopOrLaptop &&
+        array2
+        }
+        { isTabletOrMobile &&
+        array2_res
+        }
       </div>
     )
   }
